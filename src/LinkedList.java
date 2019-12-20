a5c5602161d5df639903188b6d630b04e0629ad6
Alex-Kamenev <akamenev64@gmail.com>
2019-12-20 18:58:52 -0500

    Add files via upload

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -256,7 +256,7 @@ public class LinkedList<AnyType> implements Iterable<AnyType>
           prev = cur;
           cur = cur.next;
        }
-       //insert between cur and prev
+       //insert between prev and cur
        prev.next = new Node<AnyType>(key, cur);  
     }
     
